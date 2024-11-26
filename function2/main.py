12894b5cac147bda4ff0b220b7831d44a0012e3b
vantanhly <tanh.ly@dac-datatech.vn>
2024-11-26 16:55:05 +0700

    test for basic ci

---
+++ function2/main.py
@@ -0,0 +1,10 @@
+import base64
+import functions_framework
+
+
+@functions_framework.cloud_event
+def function2(cloud_event):
+    """Handle Pub/Sub messages."""
+    pubsub_message = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
+
+    print(pubsub_message)
